4492caa005c539a2e203ad7442ba7b6f387c2c14
PeeHaa <info@pieterhordijk.com>
2014-11-16 23:09:35 +0100

    Made new posts log dynamic

--- src/Feedr/Storage/Database/Log.php
+++ src/Feedr/Storage/Database/Log.php
@@ -52,21 +52,46 @@ class Log
 
         $stmt = $this->dbConnection->prepare($query);
         $stmt->execute([
-            'userid' => $userId,
+            'userid'   => $userId,
         ]);
 
-        $recordset = $stmt->fetchAll();
+        $logs1 = $stmt->fetchAll();
+
+        $query = 'SELECT log.id AS logid, log.type, log.timestamp, posts.id AS postid';
+        $query.= ' FROM log, posts, feeds_repositories, admins';
+        $query.= ' WHERE posts.id = log.post_id';
+        $query.= ' AND feeds_repositories.id = posts.feed_repository_id';
+        $query.= ' AND admins.feed_id = feeds_repositories.feed_id';
+        $query.= ' AND admins.user_id = :userid';
+        $query.= ' ORDER BY log.id DESC LIMIT 10 OFFSET 0';
+
+        $stmt = $this->dbConnection->prepare($query);
+        $stmt->execute([
+            'userid'   => $userId,
+        ]);
+
+        $logs2 = $stmt->fetchAll();
+
+        $recordset = array_merge($logs1, $logs2);
 
         if (!$recordset) {
             return [];
         }
 
+        usort($recordset, function($a, $b) {
+            return $b['logid'] - $a['logid'];
+        });
+
         $result = [];
 
-        foreach ($recordset as $record) {
+        foreach ($recordset as $index => $record) {
             $record['timestamp'] = $timeFormatter->calculate(new \DateTime($record['timestamp']));
 
             $result[$record['logid']] = $record;
+
+            if ($index === 9) {
+                break;
+            }
         }
 
         return $result;

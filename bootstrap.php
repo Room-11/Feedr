d6c41b5ef0dca671fcb7b037c90834ec0af57f6e
Pieter Hordijk <info@pieterhordijk.com>
2015-12-20 13:17:44 +0100

    Rewritten bootstrap file to use new dependencies

--- bootstrap.php
+++ bootstrap.php
@@ -1,35 +1,37 @@
-<?php
+<?php declare(strict_types=1);
 /**
- * Bootstrap the project
+ * Bootstrap of the project
  *
- * PHP version 5.5
+ * PHP version 7.0
  *
+ * @category   Feedr
  * @author     Pieter Hordijk <info@pieterhordijk.com>
- * @copyright  Copyright (c) 2013 Pieter Hordijk <https://github.com/PeeHaa>
- * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @copyright  Copyright (c) 2015 Pieter Hordijk <https://pieterhordijk.com>
+ * @license    See the LICENSE file
  * @version    1.0.0
  */
-use Feedr\Storage\Session;
-use Feedr\Security\Generator\Factory;
-use Feedr\Security\CsrfToken;
-use Feedr\Storage\Database\Auth;
-use Feedr\Auth\User;
-use Feedr\Storage\ImmutableArray;
-use Feedr\Network\Http\Request;
-use Feedr\Router\Factory as RouterFactory;
-use Feedr\Presentation\Html;
-use Feedr\Presentation\Xml;
-use Feedr\Router\NotFoundException;
+namespace Feedr;
 
-use OAuth\ServiceFactory;
-use OAuth\Common\Http\Uri\UriFactory;
-use OAuth\Common\Storage\Session as OauthSession;
-use OAuth\Common\Consumer\Credentials;
+use CodeCollab\Http\Request\Request;
+use CodeCollab\Http\Session\Native as Session;
+use CodeCollab\Authentication\User;
+use CodeCollab\Encryption\Defuse\Decryptor;
+use CodeCollab\Router\Router;
+use FastRoute\RouteCollector;
+use FastRoute\RouteParser\Std as RouteParser;
+use FastRoute\DataGenerator\GroupCountBased as RouteDataGenerator;
+use FastRoute\Dispatcher\GroupCountBased as RouteDispatcher;
+use CodeCollab\Router\Injector;
+use CodeCollab\Router\FrontController;
+use CodeCollab\Theme\Theme;
+use CodeCollab\I18n\FileTranslator;
+use Minifine\Factory as MinifineFactory;
+use Auryn\Injector as Auryn;
 
 /**
- * Autoload composer dependencies
+ * Setup the project autoloader
  */
-require __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 /**
  * Setup the environment
@@ -37,88 +39,98 @@ require __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/init.deployment.php';
 
 /**
- * Start the session
- */
-session_start();
-
-/**
- * Setup CSRF token
- */
-$sessionStorage = new Session();
-$csrfToken      = new CsrfToken($sessionStorage, new Factory());
-
-/**
- * Setup the GitHub service
- */
-$serviceFactory = new ServiceFactory();
-$storage = new OauthSession();
-
-/**
- * Return when on CLI
+ * Prevent further execution when on CLI
  */
 if (php_sapi_name() === 'cli') {
     return;
 }
 
 /**
- * Request access token
+ * Setup decryptor
  */
-$uriFactory     = new UriFactory();
-$currentUri     = $uriFactory->createFromSuperGlobalArray($_SERVER);
-$currentUri->setQuery('');
-$credentials = new Credentials(
-    $githubCredentials['key'],
-    $githubCredentials['secret'],
-    $currentUri->getAbsoluteUri()
-);
-$github = $serviceFactory->createService('GitHub', $credentials, $storage, array());
-
-/**
- * Setup the authentication object
- */
-$user = new User($sessionStorage, new Auth($dbConnection));
-
-/**
- * Setup the HTML template renderer
- */
-$htmlTemplate = new Html(__DIR__ . '/templates', 'page.phtml');
-
-/**
- * Setup the XML template renderer
- */
-$xmlTemplate = new Xml(__DIR__ . '/templates');
+$decryptor = new Decryptor(file_get_contents(__DIR__ . '/encryption.key'));
 
 /**
  * Setup the request object
  */
-$request = new Request(
-    new ImmutableArray([]),
-    new ImmutableArray($_GET),
-    new ImmutableArray($_POST),
-    new ImmutableArray($_SERVER),
-    new ImmutableArray($_FILES),
-    new ImmutableArray($_COOKIE)
-);
+$request = new Request($decryptor, $_SERVER, $_GET, $_POST, $_FILES, $_COOKIE, file_get_contents('php://input'));
+
+/**
+ * Setup the session
+ */
+$session = new Session('/', $request->server('SERVER_NAME'), $request->isEncrypted());
+
+/**
+ * Setup authentication object
+ */
+$user = new User($session);
 
 /**
  * Setup the router
  */
-$router = (new RouterFactory())->build();
+$routeCache     = $user->isLoggedIn() ? '/cache/routes-authenticated.php' : '/cache/routes.php';
+$routeCollector = new RouteCollector(new RouteParser(), new RouteDataGenerator());
+
+$router = new Router($routeCollector, function($dispatchData) {
+    return new RouteDispatcher($dispatchData);
+}, __DIR__ . $routeCache, !$production);
 
 /**
- * Load the routes
+ * Load routes
  */
 require_once __DIR__ . '/routes.php';
 
 /**
- * Dispatch the request
+ * Setup i18n
  */
-try {
-    $route = $router->getRoute($request);
+$translator = new FileTranslator(__DIR__ . '/texts', 'en_US');
 
-    $callback = $route->getCallback();
+/**
+ * Setup the minifier
+ */
+$minifier = (new MinifineFactory())->build(__DIR__ . '/public', $production);
 
-    echo $callback($route);
-} catch(NotFoundException $e) {
-    echo '404';
-}
+/**
+ * Setup DI
+ */
+$auryn    = new Auryn();
+$injector = new Injector($auryn);
+
+/**
+ * Setup shared instances and aliases
+ */
+$auryn->share($request);
+$auryn->share($session);
+$auryn->share($user);
+$auryn->share($minifier);
+$auryn->share($translator);
+$auryn->share($decryptor);
+$auryn->define('CodeCollab\Encryption\Defuse\Encryptor', [':key' => file_get_contents(__DIR__ . '/encryption.key')]);
+$auryn->define('CodeCollab\Http\Cookie\Factory', [':domain' => $request->server('SERVER_NAME'), ':secure' => $request->isEncrypted()]);
+$auryn->define('CodeCollab\Theme\Theme', [':themePath' => __DIR__ . '/themes', ':theme' => 'AdminLTE']);
+$auryn->define('Feedr\Presentation\Template\Html', [':basePage' => '/page.phtml']);
+$auryn->alias('CodeCollab\CsrfToken\Token', 'CodeCollab\CsrfToken\Handler');
+$auryn->alias('CodeCollab\Authentication\Authentication', 'CodeCollab\Authentication\User');
+$auryn->alias('CodeCollab\CsrfToken\Storage\Storage', 'Demo\Storage\TokenSession');
+$auryn->alias('CodeCollab\Http\Session\Session', 'CodeCollab\Http\Session\Native');
+$auryn->alias('CodeCollab\CsrfToken\Generator\Generator', 'CodeCollab\CsrfToken\Generator\RandomBytes32');
+$auryn->alias('CodeCollab\I18n\Translator', 'CodeCollab\I18n\FileTranslator');
+$auryn->alias('CodeCollab\Theme\Loader', 'CodeCollab\Theme\Theme');
+$auryn->alias('CodeCollab\Template\Html', 'Demo\Presentation\Template\Html');
+$auryn->alias('CodeCollab\Encryption\Encryptor', 'CodeCollab\Encryption\Defuse\Encryptor');
+$auryn->alias('CodeCollab\Encryption\Decryptor', 'CodeCollab\Encryption\Defuse\Decryptor');
+
+/**
+ * Setup the front controller
+ */
+$frontController = new FrontController(
+    $router,
+    $auryn->make('CodeCollab\Http\Response\Response', ['server' => $request]),
+    $session,
+    $injector
+);
+
+/**
+ * Run the application
+ */
+$frontController->run($request);
